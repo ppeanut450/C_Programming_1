db045fa57c8144b62ae6cfa1e74a723c03cd092c
ppeanut450 <ppeanut450@gmail.com>
2020-10-25 00:44:25 +0900

    Q7

--- Chapter 7/Chapter 7/7.c
+++ Chapter 7/Chapter 7/7.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 int main(void)
 {
-	int x, y;
+	int i, j, n = 0;
+	for (i = 2; i <= 100; i++)
+	{
+		n = 0;
+		for (j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				n = 1;
+		}
+		if(n==0)
+			printf("%d ", i);
+	}
+	return 0;
 }
